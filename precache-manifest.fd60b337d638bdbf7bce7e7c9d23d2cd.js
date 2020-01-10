94e5f1ed5a2898016aecf01f7f0f6cb64cdd0313
Anoop Lath <Anoop.Lath@idc.bedbath.com>
2020-01-10 13:01:11 +0530

    Updates

---
+++ precache-manifest.fd60b337d638bdbf7bce7e7c9d23d2cd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f865ed4c87388f4fb8b1f42d04b99856",
+    "url": "/appstreet-ekart/index.html"
+  },
+  {
+    "revision": "07e9dc788a7d079b00b4",
+    "url": "/appstreet-ekart/static/css/2.13d70b4c.chunk.css"
+  },
+  {
+    "revision": "fc53d33323cf8397bf12",
+    "url": "/appstreet-ekart/static/css/main.83d781fe.chunk.css"
+  },
+  {
+    "revision": "07e9dc788a7d079b00b4",
+    "url": "/appstreet-ekart/static/js/2.8dbaa5c8.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/appstreet-ekart/static/js/2.8dbaa5c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc53d33323cf8397bf12",
+    "url": "/appstreet-ekart/static/js/main.8c833a02.chunk.js"
+  },
+  {
+    "revision": "4bfd07ae7da03a9db206",
+    "url": "/appstreet-ekart/static/js/runtime-main.49ffde00.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/appstreet-ekart/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/appstreet-ekart/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/appstreet-ekart/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/appstreet-ekart/static/media/slick.f97e3bbf.svg"
+  }
+]);
